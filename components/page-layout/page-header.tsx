b3e5220ae28a82c260c189fc4d1c54701692131c
Dale Gacusan <dale@xpay.world>
2022-05-04 01:09:45 +0800

    Made header menu responsive

--- components/page-layout/page-header.tsx
+++ components/page-layout/page-header.tsx
@@ -9,10 +9,14 @@ import {
   Menu,
   ActionIcon,
   Group,
+  Burger,
+  Transition,
+  Paper,
 } from "@mantine/core";
 import { useAuth } from "../../ contexts/authUserContext";
 import { useRouter } from "next/router";
 import { Logout, User } from "tabler-icons-react";
+import { useBooleanToggle } from "@mantine/hooks";
 import HelpModal from "../modals/help-modal";
 import Link from "next/link";
 
@@ -29,6 +33,22 @@ const useStyles = createStyles((theme) => ({
     justifyContent: "space-between",
     alignItems: "center",
     height: "100%",
+  },
+
+  dropdown: {
+    position: "absolute",
+    top: HEADER_HEIGHT,
+    left: 0,
+    right: 0,
+    zIndex: 0,
+    borderTopRightRadius: 0,
+    borderTopLeftRadius: 0,
+    borderTopWidth: 0,
+    overflow: "hidden",
+
+    [theme.fn.largerThan("sm")]: {
+      display: "none",
+    },
   },
 
   link: {
@@ -52,14 +72,113 @@ const useStyles = createStyles((theme) => ({
           : theme.colors.gray[0],
     },
   },
+
+  links: {
+    [theme.fn.smallerThan("sm")]: {
+      display: "none",
+    },
+  },
+
+  burger: {
+    [theme.fn.largerThan("sm")]: {
+      display: "none",
+    },
+  },
 }));
 
 export function PageHeader() {
   const { classes } = useStyles();
   const { authUser, loading, signOut } = useAuth();
   const router = useRouter();
+  const [opened, toggleOpened] = useBooleanToggle(false);
 
   const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
+
+  const defaultHeaderItems = [
+    <>
+      <a className={classes.link} onClick={() => router.push("/")}>
+        Home
+      </a>
+    </>,
+    <>
+      <a className={classes.link} onClick={() => router.push("/leaderboards")}>
+        Leaderboards
+      </a>
+    </>,
+    <>
+      <a className={classes.link} onClick={() => setIsOpenModal(true)}>
+        Help
+      </a>
+    </>,
+  ];
+  const headerItemsExpanded = [
+    <>{loading && !authUser && <Loader size="sm" />}</>,
+    <>
+      {!loading && !authUser && (
+        <a className={classes.link} onClick={() => router.push("/login")}>
+          Sign in
+        </a>
+      )}
+    </>,
+    <>
+      {!loading && authUser && (
+        <Menu
+          control={
+            <ActionIcon>
+              <Avatar
+                src={authUser.photoUrl}
+                alt={authUser.displayName as string}
+                radius="xl"
+              />
+            </ActionIcon>
+          }
+          ml={8}
+          gutter={12}
+          placement="end"
+          withArrow
+        >
+          <Menu.Label>{authUser.username}</Menu.Label>
+
+          <Menu.Item
+            icon={<User size={14} />}
+            onClick={() => router.push("/account")}
+          >
+            My Account
+          </Menu.Item>
+
+          <Menu.Item
+            color="red"
+            icon={<Logout size={14} />}
+            onClick={() => signOut()}
+          >
+            Sign out
+          </Menu.Item>
+        </Menu>
+      )}
+    </>,
+  ];
+  const headerItemsCollapsed = [
+    <>{loading && !authUser && <Loader size="sm" />}</>,
+    <>
+      {!loading && !authUser && (
+        <a className={classes.link} onClick={() => router.push("/login")}>
+          Sign in
+        </a>
+      )}
+    </>,
+    <>
+      {!loading && authUser && (
+        <>
+          <a className={classes.link} onClick={() => router.push("/account")}>
+            My account
+          </a>
+          <a className={classes.link} onClick={() => signOut()}>
+            Sign out
+          </a>
+        </>
+      )}
+    </>,
+  ];
 
   return (
     <Header height={HEADER_HEIGHT} mb={70} className={classes.root}>
@@ -71,63 +190,24 @@ export function PageHeader() {
           </Title>
         </Link>
 
-        <Group spacing={5}>
-          <a className={classes.link} onClick={() => router.push("/")}>
-            Home
-          </a>
-          <a
-            className={classes.link}
-            onClick={() => router.push("/leaderboards")}
-          >
-            Leaderboards
-          </a>
-          <a className={classes.link} onClick={() => setIsOpenModal(true)}>
-            Help
-          </a>
-
-          {loading && !authUser && <Loader size="sm" />}
-
-          {!loading && !authUser && (
-            <a className={classes.link} onClick={() => router.push("/login")}>
-              Sign in
-            </a>
-          )}
-
-          {!loading && authUser && (
-            <Menu
-              control={
-                <ActionIcon>
-                  <Avatar
-                    src={authUser.photoUrl}
-                    alt={authUser.displayName as string}
-                    radius="xl"
-                  />
-                </ActionIcon>
-              }
-              ml={8}
-              gutter={12}
-              placement="end"
-              withArrow
-            >
-              <Menu.Label>{authUser.username}</Menu.Label>
-
-              <Menu.Item
-                icon={<User size={14} />}
-                onClick={() => router.push("/account")}
-              >
-                My Account
-              </Menu.Item>
-
-              <Menu.Item
-                color="red"
-                icon={<Logout size={14} />}
-                onClick={() => signOut()}
-              >
-                Sign out
-              </Menu.Item>
-            </Menu>
-          )}
+        <Group spacing={5} className={classes.links}>
+          {defaultHeaderItems.concat(headerItemsExpanded)}
         </Group>
+
+        <Burger
+          opened={opened}
+          onClick={() => toggleOpened()}
+          className={classes.burger}
+          size="sm"
+        />
+
+        <Transition transition="pop-top-right" duration={200} mounted={opened}>
+          {(styles) => (
+            <Paper className={classes.dropdown} withBorder style={styles}>
+              {defaultHeaderItems.concat(headerItemsCollapsed)}
+            </Paper>
+          )}
+        </Transition>
       </Container>
     </Header>
   );
