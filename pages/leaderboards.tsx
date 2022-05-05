e3ab83f506c5a55c7db25a37383c81dcf0d55f82
Dale Gacusan <dale@xpay.world>
2022-05-05 21:09:10 +0800

    Change leaderboards to CSR instead of SSR to show loading state

--- pages/leaderboards.tsx
+++ pages/leaderboards.tsx
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Loader, Stack, Text, Title } from "@mantine/core";
 import { Game } from "../interfaces/game.interface";
 import { QUERY_GAMES } from "../utils/http";
 import { QueryOrderDirection } from "../utils/api/enums/query-order-direction.enum";
 import { useModals } from "@mantine/modals";
+import { useAuth } from "../ contexts/authUserContext";
+import { AlertCircle } from "tabler-icons-react";
+import { showNotification } from "@mantine/notifications";
+import { ApiResultStatus } from "../utils/api/enums/api-result-status.enum";
 import GameRecordModalContent from "../components/modal-content/game-record-modal-content";
 import GameRecord from "../components/game-record";
 
-interface LeaderboardsProps {
-  games: Game[];
-}
-
-const Leaderboards = (props: LeaderboardsProps) => {
+const Leaderboards = () => {
+  const { authUser, loading } = useAuth();
   const modals = useModals();
+  const [games, setGames] = useState<Game[]>();
 
   const openGameRecordModal = (game: Game) => {
     if (game) {
@@ -23,6 +25,44 @@ const Leaderboards = (props: LeaderboardsProps) => {
     }
   };
 
+  useEffect(() => {
+    if (!loading && authUser) {
+      QUERY_GAMES(10, {
+        direction: QueryOrderDirection.DESC,
+        fieldPath: "score",
+      })
+        .then((data) => {
+          if (data.resultInfo.resultStatus === ApiResultStatus.FAILURE) {
+            showNotification({
+              id: "fail-query-leaderboard-games",
+              autoClose: 5000,
+              title: "Something went wrong.",
+              message: `Failed to retrieve records. Ref: ${data.resultInfo.resultCode}`,
+              color: "red",
+              icon: <AlertCircle size={16} />,
+            });
+
+            setGames([]);
+          } else {
+            const { games } = data;
+
+            //@ts-ignore
+            setGames(games);
+          }
+        })
+        .catch(() => {
+          showNotification({
+            id: "fail-query-leaderboard-games",
+            autoClose: 5000,
+            title: "Something went wrong.",
+            message: "Failed to retrieve records.",
+            color: "red",
+            icon: <AlertCircle size={16} />,
+          });
+        });
+    }
+  }, [loading]);
+
   return (
     <Box mb={70}>
       <Title order={2}>Leaderboards</Title>
@@ -31,15 +71,15 @@ const Leaderboards = (props: LeaderboardsProps) => {
       </Text>
 
       <Box mt={30}>
-        {!props.games && <Loader size="sm" />}
+        {!games && <Loader size="sm" />}
 
-        {props.games && props.games.length === 0 && (
+        {games && games.length === 0 && (
           <Text color="dimmed">No available data</Text>
         )}
 
-        {props.games && props.games.length !== 0 && (
+        {games && games.length !== 0 && (
           <Stack>
-            {props.games.map((game: Game, idx: number) => {
+            {games.map((game: Game, idx: number) => {
               return (
                 <GameRecord
                   key={game.id}
@@ -58,14 +98,3 @@ const Leaderboards = (props: LeaderboardsProps) => {
 };
 
 export default Leaderboards;
-
-export async function getServerSideProps() {
-  const { games } = await QUERY_GAMES(10, {
-    direction: QueryOrderDirection.DESC,
-    fieldPath: "score",
-  });
-
-  return {
-    props: { games },
-  };
-}
