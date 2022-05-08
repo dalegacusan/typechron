2865b87489fb0a3b52491a315bac26b51215c47c
Dale Gacusan <dale@xpay.world>
2022-05-08 20:58:36 +0800

    firebase admin creds are now stored as JSON instead of base64

--- config/firebase-admin.ts
+++ config/firebase-admin.ts
@@ -1,13 +1,10 @@
 const admin = require("firebase-admin");
-import { FromBase64 } from "../utils/base64";
 
 if (!admin.apps.length) {
   admin.initializeApp({
     // @ref https://stackoverflow.com/a/48834975/12278028
     credential: admin.credential.cert(
-      JSON.parse(
-        FromBase64(process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT_KEY as string)
-      )
+      JSON.parse(process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT_KEY as string)
     ),
   });
 }
