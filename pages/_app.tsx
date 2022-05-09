d33d59a7b1c7e4ad89b6d3a0e3e71caf5d4b8d3c
Dale Gacusan <dale@xpay.world>
2022-05-09 10:13:51 +0800

    added more meta tags for better display when sharing link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,32 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Typechron</title>
+        <meta name="title" content="Typechron" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="A fast-paced single player game where you type as many words as you can within 10 seconds."
+        />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://typechron.vercel.app/" />
+        <meta property="og:title" content="Typechron" />
+        <meta
+          property="og:description"
+          content="A fast-paced single player game where you type as many words as you can within 10 seconds."
+        />
+        <meta property="og:image" content="/typechron-banner.png" />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://typechron.vercel.app/" />
+        <meta property="twitter:title" content="Typechron" />
+        <meta
+          property="twitter:description"
+          content="A fast-paced single player game where you type as many words as you can within 10 seconds."
+        />
+        <meta property="twitter:image" content="/typechron-banner.png" />
 
         <link rel="shortcut icon" href="/typechron-icon.png" />
       </Head>
